d269d18b78fa32c79557a751294ba295dc3fa170
Phillip Roux <phillip.roux@outlook.com>
2019-08-13 09:24:57 +0200

    Initial commit

---
+++ Controllers/SimulateController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fika.Web.Controllers
+{
+    public class SimulateController : Controller
+    {
+        // GET: Simulate
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WebAffinity()
+        {
+            HttpContext.Response.Cookies.Append("ARRAffinity", Guid.NewGuid().ToString());
+            return new RedirectResult("/");
+        }
+
+        public ActionResult FrontDoorAffinity()
+        {
+            HttpContext.Response.Cookies.Append("ASLBSA", Guid.NewGuid().ToString());
+            return new RedirectResult("/");
+        }
+
+        public ActionResult LoginAsCustomer(string customerId)
+        {
+            HttpContext.Response.Cookies.Append("CustomerId", customerId);
+            return new RedirectResult("/");
+        }
+    }
+}
---
+++ Pages/Blue.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fika.Web.Pages
+{
+    public class BlueModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
